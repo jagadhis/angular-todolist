8669cd0aa9506bfee8debe8aaad3e025e81f2647
jagadhis <jwaran78@gmail.com>
2022-12-20 16:24:42 +0530

    Fixed the Todo And bootstrap Nav

--- src/app/Mycomponents/todos/todos.component.ts
+++ src/app/Mycomponents/todos/todos.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
-
+export class TodosComponent implements OnInit {
+todos:Todo[];
+constructor(){
+  this.todos = [
+    {
+      sno:3,
+      title:"Learn Angular",
+      desc:"Description",
+      active:true
+    },
+    {
+      sno:4,
+      title:"Learn Angular",
+      desc:"Description",
+      active:true
+    },  {
+      sno:5,
+      title:"Learn Angular",
+      desc:"Description",
+      active:true
+    }
+  ]
+}
+ngOnInit(): void {
+ 
+}
 }
