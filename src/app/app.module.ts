ca4b3ff8bb910e3c41d7265943264f0621ef9569
jagadhis <jwaran78@gmail.com>
2022-12-20 18:36:17 +0530

    Added Todo FOm for adding Todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './Mycomponents/todos/todos.component';
 import { TodoItemComponent } from './Mycomponents/todo-item/todo-item.component';
+import { AddTodosComponent } from './Mycomponents/add-todos/add-todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    AddTodosComponent
   ],
   imports: [
     BrowserModule,
